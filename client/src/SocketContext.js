5d49f181824259f4ca88ae3485364001a16b4e34
zsoe <zwemsoe@gmail.com>
2021-03-29 22:51:15 -0700

    gcloud deploy

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -1,5 +1,10 @@
 import { createContext } from "react";
 import ioClient from "socket.io-client";
 
-export const socket = ioClient("http://localhost:5000", {});
+export const socket = ioClient(
+  process.env.NODE_ENV === "production"
+    ? "https://gcloud-test-app-309202.wl.r.appspot.com/"
+    : "http://localhost:5000",
+  {}
+);
 export const SocketContext = createContext(socket);
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const path = require("path");
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`listening on ${port}`);
@@ -12,14 +13,22 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+app.use("/", express.static(path.join(__dirname + "public")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://gcloud-test-app-309202.wl.r.appspot.com/"
+        : "http://localhost:3000",
   },
 });
 
